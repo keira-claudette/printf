f71b54c43e3185d68d2173771b75fc6f583b8638
rabbice <iankater@gmail.com>
2021-03-16 11:28:29 +0000

    first commit

---
+++ print_hex.c
@@ -0,0 +1,64 @@
+#include "holberton.h"
+
+/**
+ * print_hex - prints an unsigned int in hexidecimal form
+ * @n: unsigned int to print
+ * @c: flag to determine case of printing (0 = lower, 1 = upper)
+ *
+ * Return: number of digits printed
+ */
+int print_hex(unsigned int n, unsigned int c)
+{
+	unsigned int a[8];
+	unsigned int i, m, sum;
+	char diff;
+	int count;
+
+	m = 268435456; /* (16 ^ 7) */
+	if (c)
+		diff = 'A' - ':';
+	else
+		diff = 'a' - ':';
+	a[0] = n / m;
+	for (i = 1; i < 8; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, sum = 0, count = 0; i < 8; i++)
+	{
+		sum += a[i];
+		if (sum || i == 7)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + diff + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+/**
+ * print_x - takes an unsigned int and prints it in lowercase hex notation
+ * @x: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
+
+/**
+ * print_X - takes am unsigned int and prints it in uppercase hex notation
+ * @X: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
+
+/* add code here */
